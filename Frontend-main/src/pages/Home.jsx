2ecdac64de9e3470827ea21b0ba9dc96ed511fb1
4bjith <abhijithpops1890@gmail.com>
2025-12-05 11:53:32 +0530

    created router

---
+++ Frontend-main/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+    <div className="max-w-screen ">
+        <div className="w-full h-[70vh] bg-green-700">
+           
+        </div>
+    </div>
+  )
+}
